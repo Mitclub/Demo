0804659cbe6b36cfff5ae7fdd78f358df4b427b2
mit <mit.club@yandex.com>
2018-09-30 22:31:12 +0800

    Demo_BCEX_Trading.sln

---
+++ Demo_BCEX_Trading/Demo_BCEX_Trading/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_BCEX_Trading
+{
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+           
+            CSettings.Init();
+
+            CSimulator simulator = new CSimulator();
+
+            simulator.Run();
+
+            Console.ReadKey();
+        }
+    }
+}
