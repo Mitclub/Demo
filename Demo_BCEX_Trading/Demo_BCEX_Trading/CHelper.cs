e5041136b7c4343fb8bb87715bebd962df84abc8
mit <mit.club@yandex.com>
2018-10-12 15:31:23 +0800

    CHelper.cs

---
+++ Demo_BCEX_Trading/Demo_BCEX_Trading/CHelper.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_BCEX_Trading
+{
+    public class CHelper
+    {
+        public static int GetInteger(double dData)
+        {
+            int iRet = 0;
+            string strD = Math.Abs(dData).ToString();
+            string[] strs = strD.Split('.');
+            int.TryParse(strs[0], out iRet);
+            return iRet;
+
+        }
+}
+    public enum TRADETYPE
+    {
+        NONE = -1,
+        SELL = 0,
+        BUY = 1
+    }
+}
+
