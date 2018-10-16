067fc5378d52cce3e608c71f6a9e0db9a5872316
mit <mit.club@yandex.com>
2018-10-16 12:08:44 +0800

    update CHelper

--- Demo_BCEX_Trading/Demo_BCEX_Trading/CHelper.cs
+++ Demo_BCEX_Trading/Demo_BCEX_Trading/CHelper.cs
@@ -36,6 +36,35 @@ namespace Demo_BCEX_Trading
 
             return Is24Hours(dtRefundDate, dt);
         }
+		
+        public static bool Is24Hours(DateTime dtRefundDate, DateTime dt)
+        {
+            bool bret = false;
+
+            TimeSpan ts = dtRefundDate - dt;
+
+            if (ts.TotalHours >= 24)
+            {
+                return true;
+            }
+            return bret;
+        }
+
+        /// <summary>
+        /// 
+        /// 根据账号MIT数量余额计算返利利率
+        /// 
+        /// </summary>
+        /// <param name="dOriginalNumber">账号起始总MIT数量</param>
+        /// <param name="dCurrentNumber">当前已经销售MIT的数量</param>
+        /// <param name="dStartRefundRate">起始返利基准利率</param>
+        /// <returns></returns>
+        public static double CalcRefundRate(double dOriginalNumber, double dCurrentNumber, double dStartRefundRate)
+        {
+            double dRet = GetInteger(dCurrentNumber) / GetInteger(dOriginalNumber * dStartRefundRate) * dStartRefundRate + dStartRefundRate;
+
+            return dRet > 0.9 ? 0.9 : dRet;
+        }
 
 }
     public enum TRADETYPE
