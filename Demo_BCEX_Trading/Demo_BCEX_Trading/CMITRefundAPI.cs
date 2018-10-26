0bcfed45ec2a3d948ade6b9565944c2b02f99af7
Mitclub <mit.club@yandex.com>
2018-10-26 16:46:17 +0800

    CMITRefundAPI.cs  001

---
+++ Demo_BCEX_Trading/Demo_BCEX_Trading/CMITRefundAPI.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_BCEX_Trading
+{
+    /// <summary>
+    /// 
+    /// 本类按照Tony的思路来做，分红是按24小时为一个间隔，时间不累计，权重不计算时间因素？
+    /// 
+    /// </summary>
+    public class CMITRefundAPI
+    {
+        private List<MITUserTradeRecs> _lstWeightDetails = new List<MITUserTradeRecs>();
+        //获得指定分红日的昨日MIT项目方获得ETH总数
+        public double GetYesterdayTotalProfit(DateTime dtRefundDate)
+        {
+#if DEBUG
+            return CSettings.YesterdayTotalProfit;
+#endif
+            return CBCEXTradeAPI.GetProfit(dtRefundDate.AddDays(-1));
+        }
+
+        //获得指定分红日的前天MIT项目方获得ETH总数
+        public double GetTheDayBeforeYesterdayTotalProfit(DateTime dtRefundDate)
+        {
+#if DEBUG
+            return CSettings.TheDayBeforeYesterdayTotalProfit;
+#endif
+            return CBCEXTradeAPI.GetProfit(dtRefundDate.AddDays(-2));
+        }
+
+    }
+
+
+}
