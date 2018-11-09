959805cdc1b3836b80b6d617f5f2ba140f96b3c9
Mitclub <mit.club@yandex.com>
2018-11-09 13:10:04 +0800

    public double GetTheDayBeforeYesterdayTotalProfit(DateTime dtRefundDate) public double GetFloatingRate(double dYesterdayData, double dTheDayBeforeYesterdayData, double dBaseFloatingRate)

--- Demo_BCEX_Trading/Demo_BCEX_Trading/CMITRefundAPI2.cs
+++ Demo_BCEX_Trading/Demo_BCEX_Trading/CMITRefundAPI2.cs
@@ -26,6 +26,44 @@ namespace Demo_BCEX_Trading
             return CBCEXTradeAPI.GetProfit(dtRefundDate.AddDays(-1));
         }
 
+        //获得指定分红日的前天MIT项目方获得ETH总数
+        public double GetTheDayBeforeYesterdayTotalProfit(DateTime dtRefundDate)
+        {
+#if DEBUG
+            return CSettings.TheDayBeforeYesterdayTotalProfit;
+#endif
+            return CBCEXTradeAPI.GetProfit(dtRefundDate.AddDays(-2));
+        }
+
+        /// <summary>
+        /// 
+        /// 获得浮动分红百分比，本函数实现交易模式中公式1和公式2
+        /// 
+        /// </summary>
+        /// <param name="dYesterdayData">获得昨日MIT项目方获得ETH总数</param>
+        /// <param name="dTheDayBeforeYesterdayData">获得前天MIT项目方获得ETH总数</param>
+        /// <param name="dBaseFloatingRate">基准浮动百分比</param>
+        /// <returns></returns>
+        public double GetFloatingRate(double dYesterdayData, double dTheDayBeforeYesterdayData, double dBaseFloatingRate)
+        {
+            if (dYesterdayData <= 0 || dTheDayBeforeYesterdayData <= 0 || dYesterdayData == dTheDayBeforeYesterdayData)
+            {
+                return 0;
+            }
+
+            //公式 1：计算昨天和前天的交易额涨跌百分比
+            double dRate = (dYesterdayData - dTheDayBeforeYesterdayData) / Math.Min(dYesterdayData, dTheDayBeforeYesterdayData);
+
+            //公式 2：交易额涨跌百分比归一化公式
+            double dMAX = CSettings.FloatRateMAX, dMIN = CSettings.FloatRateMIN;//应该从外部中获取
+            double dFloatingRate = (Math.Min(Math.Abs(dRate), dMAX) - dMIN) / (dMAX - dMIN) * dBaseFloatingRate;
+            if (dRate < 0)
+            {
+                dFloatingRate = 0 - dFloatingRate;
+            }
+            return CHelper.Round(dFloatingRate);
+        }
+
     }
 
 
