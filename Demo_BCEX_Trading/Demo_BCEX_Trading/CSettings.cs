c6d7005b65aff83ea3611c22253fb371b9ebb4b3
Mitclub <mit.club@yandex.com>
2018-12-12 14:15:36 +0800

    public DateTime GetTime(string timeStamp)

--- Demo_BCEX_Trading/Demo_BCEX_Trading/CSettings.cs
+++ Demo_BCEX_Trading/Demo_BCEX_Trading/CSettings.cs
@@ -453,6 +453,14 @@ namespace Demo_BCEX_Trading
                 lstUsers.Add(userRecs);
             }
         }
+        public DateTime GetTime(string timeStamp)
+        {
+            DateTime dtStart = TimeZone.CurrentTimeZone.ToLocalTime(new DateTime(1970, 1, 1));
+
+            long lTime = long.Parse(timeStamp + "0000000");
+            TimeSpan toNow = new TimeSpan(lTime);
+            return dtStart.Add(toNow);
+        }
 
 	}
 
